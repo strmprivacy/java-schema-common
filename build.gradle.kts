f2ef3319ad542b12533a10a4b04ea2be00d9c0e7
Robin Trietsch <robin@streammachine.io>
2021-11-09 14:25:11 +0100

    fix: fix sonatype host

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ nexusPublishing {
 
     repositories {
         sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(base64Decode("sonatypeUsername"))
             password.set(base64Decode("sonatypePassword"))
         }
